1819b67320a22800bafa49b254d3097116ebfc9d
Konstantin Parfenov <parkonst@gmail.com>
2019-11-24 03:47:13 +0300

    main things

--- webapp/forecast/views.py
+++ webapp/forecast/views.py
@@ -23,7 +23,40 @@ def create_plot_pie():
 
     return graphJSON
 
+def create_plot_forecast(avg_type):
+    #default SMA
+    period=60
+    roll=21
+    roll_func= avg_type 
+    function='mean'
+    trace=go.Scatter(
+        x=forecast_chrt_data.date.iloc[-period:],
+        y=forecast_chrt_data.pet.iloc[-period:]
+    )
+    if roll_func=='EMA':
+        y=forecast_chrt_data.pet.ewm(span=roll).agg(function).iloc[-period:]
+    if roll_func=='SMA':
+        y=forecast_chrt_data.pet.rolling(roll).agg(function).iloc[-period:]
+    trace1=go.Scatter(
+        x=forecast_chrt_data.date.iloc[-period:],
+        y=y
+    )
+    fig=go.Figure(data=[trace,trace1])
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+    
+
+
 @blueprint.route('/forecast_pie', methods = ['GET','POST'])
-def forecast():
+def forecast_pie():
     pie_chart_weights = create_plot_pie()
     return render_template('forecast/piechart.html', plot_pie_chart_weights = pie_chart_weights )
+
+
+@blueprint.route('/forecast_chart/<avg_type_main>', methods = ['GET','POST'])
+def forecast_chart(avg_type_main):
+    avg_type=avg_type_main
+    forecast_chart = create_plot_forecast(avg_type)
+    return render_template('forecast/forecast.html', plot_forecast_chart = forecast_chart , avg_type = avg_type_main)
